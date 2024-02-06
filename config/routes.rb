0f3bbac8f6751a5ecb962dd816c9332e6a6f9036
codecademy <ccuser@codecademy.com>
2024-02-06 10:05:37 +0000

    Finished first part of the project. To be continued ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#welcome'
-  get 'projects', to: 'pages#projects'
+  root 'pages#welcome', as: :welcome
+  get 'projects', to: 'pages#projects', as: :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
